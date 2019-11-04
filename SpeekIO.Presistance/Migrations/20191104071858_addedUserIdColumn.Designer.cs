5d87fc6ab79b696f95f25a12a1f574a2bf1c88aa
Adnan Shabir <adnan.shabir@synergy-it.com>
2019-11-04 12:20:00 +0500

    added UserId in profile table

--- SpeekIO.Presistance/Migrations/20191104071858_addedUserIdColumn.Designer.cs
+++ SpeekIO.Presistance/Migrations/20191104071858_addedUserIdColumn.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SpeekIO.Presistence.Context;
 
 namespace SpeekIO.Presistence.Migrations
 {
     [DbContext(typeof(SpeekIOContext))]
-    partial class SpeekIOContextModelSnapshot : ModelSnapshot
+    [Migration("20191104071858_addedUserIdColumn")]
+    partial class addedUserIdColumn
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
