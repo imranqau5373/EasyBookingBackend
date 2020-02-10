005a418775e424b39521c430ef3465423bb06cce
Zohaib Khan <zohaibkhan974@gmail.com>
2020-02-10 22:32:08 +0500

    schedular test

--- SpeekIO.Application/CommandAndQuery/BookingModule/Query/GetCompanyBookings/Dto/GetCompanyBookingListDto.cs
+++ SpeekIO.Application/CommandAndQuery/BookingModule/Query/GetCompanyBookings/Dto/GetCompanyBookingListDto.cs
@@ -12,10 +12,11 @@ namespace EasyBooking.Application.CommandAndQuery.BookingModule.Query.GetCompany
 		public string Description { get; set; }
 		public string CourtName { get; set; }
 		public string UserName { get; set; }
+		public string Style { get; set; }
 		public long? CourtId { get; set; }
 		public long? UserId { get; set; }
-		public DateTime? BookingStartTime { get; set; }
-		public DateTime? BookingEndTime { get; set; }
+		public string BookingStartTime { get; set; }
+		public string BookingEndTime { get; set; }
 		public bool IsBooked { get; set; }
 		public bool IsEmailed { get; set; }
 	}
